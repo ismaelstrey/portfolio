722a6cc40582654e31a8936f5b74e5cf8838eddc
ismael strey pereira <ismaelstrey@hotmail.com>
2025-03-21 12:10:28 -0300

    chore: add @flydotio/dockerfile dependency and update package-lock.json
    This commit adds the @flydotio/dockerfile package as a dev dependency to the backend project. The package-lock.json file has been updated to reflect this new dependency. This change is part of the setup for Dockerfile configuration and deployment.

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import {Montserrat } from "next/font/google";
+import "./globals.css";
+
+
+
+export const metadata: Metadata = {
+  title: "Portifolio",
+  description: "Portifolio Ismael Strey pereira",
+};
+const font = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${font.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
